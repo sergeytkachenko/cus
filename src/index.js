b3447e6516913cc320a75d3bf3af4bc396a28c98
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-03-30 01:40:08 +0300

    improved search selectors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import CSSSelector from './css-selector'
 import XPathSelector from './xpath-selector'
 
+function _clearFakeValues(str) {
+	return str.replace('http://elasticdata.io:81/', '');
+}
+
 export default {
 
 	generateUniqueSelector(el, win = window) {
 		const css = CSSSelector.generateUnique(el, win);
 		if (!css.startsWith('html')) {
-			return css;
+			return _clearFakeValues(css);
 		}
 		const xpath = XPathSelector.generateUnique(el, win);
 		if (!xpath.startsWith('//html')) {
-			return xpath;
+			return _clearFakeValues(xpath);
 		}
 		return null;
-	}
+	},
+
+
 };
