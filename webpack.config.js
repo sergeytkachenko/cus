b524e4f49507ff84e15569a090a553b04404f23e
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2019-01-21 01:45:53 +0200

    webpack.output.libraryTarget

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,8 @@ module.exports = {
 	},
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		library: 'CssFinder',
+		libraryTarget: 'commonjs2'
 	}
 };
